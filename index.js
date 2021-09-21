5a3eda0b5fb5b4c301846d342074dd72a968eee2
KelvinYCH <kelvin918918918@gmail.com>
2021-09-21 12:59:45 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Crawler = require("crawler");
 const querystring = require('querystring');
 
-let startTokenId = 100;
-let endTokenId = 300;
+let startTokenId = 1;
+let endTokenId = 10000;
 const contractAddress = "0x9bfa45382268e4bacbd1175395728153dc5248f2";
 
 const MAX_FETCH_UNIT = 30;
@@ -11,17 +11,25 @@ let token = [];
 let result = [];
  
 var c = new Crawler({
-    maxConnections : 10,
+    maxConnections : 3,
+    rateLimit: 1000,
     jQuery:false,
     callback : function (error, res, done) {
         if(error){
             console.log(error);
         }else{
             let json = JSON.parse(res.body);
-            json.orders.forEach((item)=>{
-                //console.log(item.asset.token_id);
-                result.push(item.asset.token_id);
-            });
+            if(json.detail){
+                console.log("error : " + json.detail);
+                console.log("retrying");
+                console.log(res.options.uri);
+                c.queue(res.options.uri);
+            } else {
+                json.orders.forEach((item)=>{
+                    console.log(item.asset.token_id);
+                    result.push(item.asset.token_id);
+                });
+            }
         }
         done();
     }
