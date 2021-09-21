756249b835d6eb2f44a98d599408fda8a0cc65fe
KelvinYCH <kelvin918918918@gmail.com>
2021-09-21 12:49:05 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const Crawler = require("crawler");
+const querystring = require('querystring');
+
+let startTokenId = 100;
+let endTokenId = 300;
+const contractAddress = "0x9bfa45382268e4bacbd1175395728153dc5248f2";
+
+const MAX_FETCH_UNIT = 30;
+
+let token = [];
+let result = [];
+ 
+var c = new Crawler({
+    maxConnections : 10,
+    jQuery:false,
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            let json = JSON.parse(res.body);
+            json.orders.forEach((item)=>{
+                //console.log(item.asset.token_id);
+                result.push(item.asset.token_id);
+            });
+        }
+        done();
+    }
+});
+
+c.on('drain',function(){
+    //console.log(result);
+    result = result.map((val)=>parseInt(val)).sort();
+    console.log(result);
+});
+
+let expectedLoop = Math.ceil((endTokenId - startTokenId + 1 ) / MAX_FETCH_UNIT);
+
+for(let i = 0; i < expectedLoop; i++){
+    token = Array.from(new Array(MAX_FETCH_UNIT),(val,index) => i * MAX_FETCH_UNIT + index + startTokenId );
+    token = token.filter((val)=>val <= endTokenId);
+    let qs = querystring.stringify({
+        asset_contract_address: contractAddress,
+        bundled: 'false',
+        include_bundled: 'false',
+        include_invalid: 'false',
+        token_ids: token,
+        side: '1',
+        limit: '50',
+        offset: '0',
+        order_by: 'created_date',
+        order_direction: 'desc'
+    });
+    c.queue("https://api.opensea.io/wyvern/v1/orders?" + qs)
+}
